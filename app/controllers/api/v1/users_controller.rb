2379aaf6bc44ab51d5eb2aa158521c625274b42b
Daniel Tinivella <dtinivella@gmail.com>
2023-02-12 20:39:06 -0300

    Adds the user create endpoint

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -6,6 +6,22 @@ module Api
       def show
         render json: User.find(params[:id])
       end
+
+      def create
+        @user = User.new(user_params)
+
+        if @user.save
+          render json: @user, status: :created
+        else
+          render json: @user.errors, status: :unprocessable_entity
+        end
+      end
+
+      private
+
+      def user_params
+        params.require(:user).permit(:email, :password_digest)
+      end
     end
   end
 end
--- test/controllers/api/v1/users_controller_test.rb
+++ test/controllers/api/v1/users_controller_test.rb
@@ -16,6 +16,20 @@ module Api
         json_response = JSON.parse(response.body)
         assert_equal @user.email, json_response['email']
       end
+
+      test 'should create user' do
+        assert_difference('User.count') do
+          post api_v1_users_url, params: { user: { email: 'test@test.or', password_digest: '123456' } }, as: :json
+        end
+        assert_response :created
+      end
+
+      test 'should not create user with taken email' do
+        assert_no_difference('User.count') do
+          post api_v1_users_url, params: { user: { email: @user.email, password_digest: '123456' } }, as: :json
+        end
+        assert_response :unprocessable_entity
+      end
     end
   end
 end
