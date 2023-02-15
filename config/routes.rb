f3d6df5ed17e5a626ceefd78c888adbe55f16d9c
Daniel Tinivella <dtinivella@gmail.com>
2023-02-14 21:20:46 -0300

    add test to test/controllers/api/v1/tokens_controller_test.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      resources :tokens, only: %i[create]
+    end
+  end
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :users, only: %i[show create update destroy]
